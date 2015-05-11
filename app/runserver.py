101f0630c3b4db6838cc9e1c00017e6abeb8ba86
alextbok <alexander_bok@brown.edu>
2015-05-11 19:20:36 -0400

    Added development vs production mode

--- app/runserver.py
+++ app/runserver.py
@@ -1,4 +1,16 @@
 from application import app
 from application.db import setup
+from optparse import OptionParser
 
-app.run(debug=True)
+parser = OptionParser()
+parser.add_option("--development", 
+		help="Run in development mode with debugging enabled on port 5000.",
+		action="store_true",
+		default=False)
+(options, args) = parser.parse_args()
+
+if __name__=="__main__":
+	if options.development:
+		app.run(debug=True)
+	else:
+		aap.run(host='0.0.0.0', port=80)
